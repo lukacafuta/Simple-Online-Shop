e82fca23d26d44acb7c75c1811ee9417e4e6058c
lukacafuta <luka.cafuta@gmail.com>
2024-06-05 13:08:32 +0200

    saving cart items to local storage

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,39 +1,65 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Helper functions to load and save state from/to localStorage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("cart");
+    return serializedState ? JSON.parse(serializedState) : [];
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("cart", serializedState);
+  } catch (error) {
+    console.error;
+  }
+};
+
 const initialState = {
-    items: [],
+  items: loadState(),
 };
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState,
-    reducers: {
-        addItem: (state, action) => {
-            const { product, quantity } = action.payload;
-            const existingItem = state.items.find(item => item.product.id === product.id);
-            if (existingItem) {
-                existingItem.quantity += quantity;
-            } else {
-                state.items.push({ product, quantity });
-            }
-        },
-        removeItem: (state, action) => {
-            state.items.splice(action.payload, 1); //remove item at the specified index
-        },
-        reduceQuantity: (state, action) => {
-            const index = action.payload;
-            if (state.items[index].quantity > 1) {
-                state.items[index].quantity -=1;
-            } else {
-                state.items.splice(index, 1);
-            }
-        },
-        increaseQuantity: (state, action) => {
-            const index = action.payload;
-            state.items[index].quantity += 1;
-        },
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const { product, quantity } = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.product.id === product.id
+      );
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        state.items.push({ product, quantity });
+      }
+      saveState(state.items);
     },
+    removeItem: (state, action) => {
+      state.items.splice(action.payload, 1); //remove item at the specified index
+      saveState(state.items);
+    },
+    reduceQuantity: (state, action) => {
+      const index = action.payload;
+      if (state.items[index].quantity > 1) {
+        state.items[index].quantity -= 1;
+      } else {
+        state.items.splice(index, 1);
+      }
+      saveState(state.items);
+    },
+    increaseQuantity: (state, action) => {
+      const index = action.payload;
+      state.items[index].quantity += 1;
+      saveState(state.items);
+    },
+  },
 });
 
-export const { addItem, removeItem, reduceQuantity, increaseQuantity } = cartSlice.actions;
+export const { addItem, removeItem, reduceQuantity, increaseQuantity } =
+  cartSlice.actions;
 export default cartSlice.reducer;
